56fbff4cbc8c77010b52fd096e4ec85d86e01b1d
asfodei <belann3011@gmail.com>
2022-12-20 14:47:45 +0300

    .

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
 import starting from '../src/cli.js';
+
 starting();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,11 @@
 import readlineSync from 'readline-sync';
 
 const starting = (text) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(text);
-    return userName;
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(text);
+  return userName;
 };
 
 export default starting;
