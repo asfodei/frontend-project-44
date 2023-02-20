6858b94dc2366eb7988aeb9e9312d5481fe4b7e2
asfodei <belann3011@gmail.com>
2023-02-20 11:44:46 +0300

    .

--- src/math.js
+++ src/math.js
@@ -51,10 +51,3 @@ export const opper4 = (str) => {
   const solve = (pastNumber + nextNumber) / 2;
   return `${solve}`;
 };
-
-export const opper5 = (nuChto) => {
-  const pr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-    41, 43, 47, 53, 59, 61, 67, 71,
-    73, 79, 83, 89, 97, 101, 103, 107, 109];
-  return (pr.includes(nuChto) ? 'yes' : 'no');
-};
